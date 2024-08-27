47d13d68cab24413d98cc62246223242bbcda3fb
Bochkareva AK <mrnatsumi8@gmail.com>
2024-08-27 22:07:25 +0400

    added task demoqa

--- src/test/java/tests/RegistrationFormPageObjectTests.java
+++ src/test/java/tests/RegistrationFormPageObjectTests.java
@@ -1,8 +1,10 @@
 package tests;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+@Tag("demoqa")
 public class RegistrationFormPageObjectTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
