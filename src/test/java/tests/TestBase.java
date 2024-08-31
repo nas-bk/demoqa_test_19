43f83ff2fdf08bece99face93f8b18b7c9034ed0
Bochkareva AK <mrnatsumi8@gmail.com>
2024-08-31 16:51:33 +0400

    fix

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -15,11 +15,11 @@ import java.util.Map;
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.browser = System.getProperty("browser","chrome");
         Configuration.browserSize = System.getProperty("browserSize","1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion","101");
         Configuration.remote = System.getProperty("remote","https://user1:1234@selenoid.autotests.cloud/wd/hub");
-        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 10000;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
