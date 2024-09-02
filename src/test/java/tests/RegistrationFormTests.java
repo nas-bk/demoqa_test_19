056bf1ef76aa578a5d01649bfb660164d8263d9b
Bochkareva AK <mrnatsumi8@gmail.com>
2024-09-02 22:55:38 +0400

    notifications

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -2,12 +2,14 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
+@Tag("notification")
 public class RegistrationFormTests {
 
     @BeforeAll
